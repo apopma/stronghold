08a5bc8493d5ac08bbd99376b170582f9a229edf
App Academy Student <student@appacademy.io>
2015-08-13 15:51:37 -0700

    add composite view add/remove methods

--- app/assets/javascripts/views/checklists/checklist_index.js
+++ app/assets/javascripts/views/checklists/checklist_index.js
@@ -1,10 +1,22 @@
 Stronghold.Views.ChecklistIndex = Backbone.CompositeView.extend ({
   template: JST['checklists/index'],
   className: 'checklist-index',
+  // model: project
+  // collection: project.checklists()
 
   initialize: function () {
     this.listenTo(this.model, "sync", this.render);
-    this.listenTo(this.collection, "sync add remove", this.render);
+    this.listenTo(this.collection, "add", this.addChecklistView);
+    this.listenTo(this.collection, "remove", this.removeChecklistView);
+  },
+
+  addChecklistView: function(checklist) {
+    var subview = new Stronghold.Views.ChecklistIndexItem({ model: checklist });
+    this.addSubview(".checklists", subview);
+  },
+
+  removeChecklistView: function(checklist) {
+    this.removeModelSubview('.checklists', checklist);
   },
 
   render: function () {
