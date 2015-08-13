a2da0ee34769750a5a39bb59b614f279a0ff6e32
App Academy Student <student@appacademy.io>
2015-08-13 09:14:34 -0700

    add routes for checklists and tasks

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@ Rails.application.routes.draw do
   get "/home" => "static_pages#home", as: :home
 
   namespace :api, defaults: { format: :json } do
-    resources :projects
-    resources :project_memberships, only: [:create, :destroy]
+    resources :projects, except: [:new, :edit] do
+      resources :checklists, only: [:index, :show]
+    end
+
+    resources :checklists, only: [:create, :update, :destroy] do
+      resources :tasks, only: [:create, :update, :destroy]
+    end
+    
+    resources :project_memberships, only: [:create, :update, :destroy]
   end
 
   resources :users, only: [:new, :create, :show, :edit, :update]
