4aa9ce13b78163e9a84ca644ddd0b35b9f8f1b10
App Academy Student <student@appacademy.io>
2015-08-14 10:12:01 -0700

    add 'admins' association to Projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,6 +7,17 @@ class Project < ActiveRecord::Base
   has_many :members, through: :project_memberships, source: :user
   has_many :checklists, dependent: :destroy
 
+  def admins
+    User.find_by_sql(<<-SQL)
+      SELECT *
+      FROM users u
+      JOIN project_memberships pm
+      ON pm.user_id = u.id
+      WHERE pm.admin IS TRUE
+      AND pm.project_id = #{self.id}
+    SQL
+  end
+
   private
   def set_creator_as_admin
     ProjectMembership.create!(user: self.creator, project: self, admin: true)
