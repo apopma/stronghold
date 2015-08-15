205405a28474693cac987b8bf1ca7624ed91af05
App Academy Student <student@appacademy.io>
2015-08-15 16:24:40 -0700

    search usernames and emails case-insensitively

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,11 @@
 class Api::UsersController < ApplicationController
   def index
+
     if params[:query].present?
       @project = Project.find(params[:project_id])
-      results_by_name = @project.members.where("username ~ ?", params[:query])
-      results_by_email = @project.members.where("email ~ ?", params[:query])
-      @users = (results_by_name.concat results_by_email).uniq!
+      results_by_name = @project.members.where("username ~* ?", params[:query])
+      results_by_email = @project.members.where("email ~* ?", params[:query])
+      @users = (results_by_name.concat results_by_email).uniq
     else
       @users = User.none
     end
